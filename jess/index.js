efeefe03ecfe5f3f2c9f4b3a569fac177c993b30
Dulal Barman <91646537+droy70@users.noreply.github.com>
2022-07-20 16:38:54 +0600

    Add files via upload

---
+++ jess/index.js
@@ -0,0 +1,18 @@
+
+ 
+
+  $(function(){
+    $('.myslider').slick({
+      prevArrow:'.arrow_priv',
+      nextArrow:'.arrow_next',
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'item',
+    });
+  });
+
+
+
+  
